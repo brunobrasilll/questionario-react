40f78cb61f090d1e1615ec72e72d2114d40cb60b
brunobrasilll <RM93263@fiap.com.br>
2022-10-19 19:07:01 -0300

    Questionario Pronto.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Questionario from './components/Questionario/Questionario'
+
+export default function App() {
+  return (
+        <div>
+            <Questionario>export default App;</Questionario>
+        </div>
+  )
+}
